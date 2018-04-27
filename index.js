2589a1ed6347219ae99f6048728e588d245f502a
Adrian Samarripas <asamarripas@gmail.com>
2018-04-27 08:21:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ function prependKitten(name){
 }
 
 function removeLastKitten(name){
-  return kitten.slice(0, 2);
+  return kittens.slice(0, kittens.length - 1);
 }
 
 function removeFirstKitten(name){
-  return kitten.slice(1, 3);
+  return kittens.slice(1, 3);
 }
